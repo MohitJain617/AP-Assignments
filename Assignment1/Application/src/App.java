3dd2590bae85b5f6d9c0597a6cd00bb3e5320cb2
Mohit Jain x64 <mohit20221@iiitd.ac.in>
2021-10-07 19:07:53 +0530

    Setting up all the classes and adding gitignore

--- Assignment1/Application/src/App.java
+++ Assignment1/Application/src/App.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class App {
+
+    public void add_vaccine(){}
+    public void register_hospital(){}
+    public void register_citizen(){}
+    public void create_slots(){}
+    public void book_slot(){}
+    public void slots_available_in_hospital(){}
+    public void vaccination_status(){}
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
     }
 }
